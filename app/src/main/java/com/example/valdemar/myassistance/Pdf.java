995b25944e3069c66b73feff8c175a38c1ae340d
unknown <amigo_diablito@hotmail.com>
2018-12-07 01:56:58 -0400

    prevista del PDF

---
+++ app/src/main/java/com/example/valdemar/myassistance/Pdf.java
@@ -0,0 +1,13 @@
+package com.example.valdemar.myassistance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pdf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf);
+    }
+}
