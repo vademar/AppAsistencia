439a72d058c131e2357ef960f300ca87b9a6c3be
unknown <amigo_diablito@hotmail.com>
2018-12-07 22:48:22 -0400

    pre vista

--- app/src/main/java/com/example/valdemar/myassistance/Pdf.java
+++ app/src/main/java/com/example/valdemar/myassistance/Pdf.java
@@ -1,27 +1,58 @@
 package com.example.valdemar.myassistance;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
 
 import java.util.ArrayList;
 
 public class Pdf extends AppCompatActivity {
     private TextView fecha;
-    private ListView LIST;
-    private ArrayList<String> LISTAS, Rows;
-    private ArrayAdapter<String> ADAPTER;
+    private TableLayout LIST;
+    private ArrayList LISTAS;
+    private String[] Rows;
+    private ArrayAdapter<String> NUM,DAT,HOR;
+    private ArrayList<String> num, dat,hor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf);
         fecha = (TextView)findViewById(R.id.Fecha);
-        LIST = (ListView)findViewById(R.id.LIS);
-        LISTAS = (ArrayList<String>) getIntent().getStringArrayListExtra("Array");
-        ADAPTER = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, LISTAS);
-        LIST.setAdapter(ADAPTER);
+        LIST = (TableLayout) findViewById(R.id.LISTAS);
+        LISTAS = (ArrayList) getIntent().getSerializableExtra("Array");
+        dat = new ArrayList<String>();
+        for(int indexR=0; indexR<LISTAS.size(); indexR++){
+            TableRow Ro=new TableRow(this);
+            String []row = (String[]) LISTAS.get(indexR);
+            TextView textView;
+            for(int indexC=0; indexC<3;indexC++){
+                textView = new TextView(this);
+                if(indexC ==2 ){
+                    textView.setWidth(245);
+                }
+                textView.setHeight(78);
+                textView.setGravity(Gravity.CENTER_VERTICAL);
+                textView.setPadding(15,15,15,15);
+                textView.setText(row[indexC]);
+                textView.setTextSize(11);
+                //textView.setTextColor(Color.BLUE);
+                Ro.addView(textView);
+            }
+            LIST.addView(Ro);
+        }
+        DAT = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, dat);
+        //LIST.setAdapter(DAT);
         llenar();
     }
     public void llenar(){
